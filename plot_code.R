794f4316a47ffc3ff85056d78eb092f1208f18f5
connermcb <connermcb@yahoo.com>
2017-12-27 23:41:37 -0600

    finalized, documented plot code

--- plot_code.R
+++ plot_code.R
@@ -48,10 +48,15 @@ dev.off()
 
 
 ## Question 3
+# open png plotting device
+png("question3.png")
+
+# group, summarize data
 ems_type <- NEI[NEI$fips==24510,]%>%
   group_by(year, type)%>%
   summarise(tot=sum(Emissions))
 
+# plot summaries
 ggplot(ems_type)+
   geom_line(aes(x=year, y=tot, color=as.factor(type)))+
   labs(title="PM2.5 Emissions by Type", 
@@ -59,34 +64,72 @@ ggplot(ems_type)+
        color="Source Type")+
   theme(plot.title = element_text(hjust=0.5))
 
+# turn png device off
+dev.off()
+
+
 ## Question 4
+# open png plotting device
+png("question4.png")
+
+# subset, group, summarize data
 coal <- full[grepl("Coal", full$Short.Name),]
 coal_sums <- coal%>%
   group_by(year)%>%
   summarize(tot=sum(Emissions))
 
-plot(coal_sums$year, coal_sums$tot, type = "l")
+# plot summaries
+plot(coal_sums$year, coal_sums$tot, type = "l", col="blue",
+     main = "Coal-Sourced PM2.5 Emissions by Year",
+     xlab = "Year", ylab = "Tons of Emissions")
+points(coal_sums$year, coal_sums$tot, pch=20, col="blue",cex=1.5)
+
+# turn off png plot device
+dev.off()
 
 ## Question 5
+# open png plotting device
+png("question5.png")
+
+# subset, group, summarize data
 cars_balt <- full[grepl("Gasoline", full$Short.Name) & full$fips=="24510",]
 cars_balt <- cars_balt%>%
   group_by(year)%>%
   summarize(tot=sum(Emissions))
-plot(cars_balt$year, cars_balt$tot, type="l")
+
+# plot summaries
+plot(cars_balt$year, cars_balt$tot, type="l", col="blue", 
+     main = "Baltimore Motor Vehicle Sourced PM2.5",
+     xlab = "Year", ylab = "Tons of Emissions")
+points(cars_balt$year, cars_balt$tot, pch=20, col="blue", cex=1.5)
+
+# turn off png device
+dev.off()
 
 ## Question 6
+# open png plotting devices
+png("question6.png")
+
+# subset, group, summarize data
 cars_la <- full[grepl("Gasoline", full$Short.Name) & 
                   full$fips %in% c("24510", "06037"),]
 cars_la <- cars_la%>%
   group_by(fips, year)%>%
   summarize(tot=sum(Emissions))
+
+# plot summaries
 ggplot(cars_la)+
-  geom_line(aes(year, tot, color=fips), show.legend = FALSE)+
+  geom_line(aes(year, tot, color=fips))+
   geom_point(aes(year, tot, color=fips), show.legend = FALSE)+
   geom_text(aes(year, tot, label=round(tot, 1)), vjust=-0.6, data = cars_la)+
   scale_y_continuous(limits = c(0,3050))+
-  theme_minimal()
+  scale_color_discrete(labels=c("LA", "Baltimore"))+
+  labs(title="Baltimore/LA PM2.5 Emissions by Year", 
+       x="Year", y="Tons of Emissions", color="City")+
+  theme(plot.title = element_text(hjust=0.5),
+        legend.position = c(.2, .5))
 
-plot(cars_la$year, cars_la$tot, type="l", col="blue")
-plot(cars_balt$year, cars_balt$tot, type="l", col="red")
-cars_la
+# turn plot device off
+dev.off()
+
+
